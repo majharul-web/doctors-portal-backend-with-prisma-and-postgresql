b252449c4f37f24c527b1797ad7fc20a16bff0ae
majharul <majharuljony163@gmail.com>
2023-09-11 14:36:04 +0600

    Done doctor and specelization part

---
+++ src/app/modules/Doctors/Doctors.services.ts
@@ -0,0 +1,79 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Doctor } from '@prisma/client'
+import prisma from '../../shared/prisma'
+
+const createDoctor = async (data: Doctor): Promise<Doctor> => {
+  const result = await prisma.doctor.create({ data })
+  return result
+}
+
+const getDoctors = async (
+  searchTerm: string,
+  sortBy: string,
+  sortOrder: 'asc' | 'desc',
+  limit: number,
+  page: number,
+  filterData: any,
+): Promise<Doctor[] | any> => {
+  const result = await prisma.doctor.findMany({
+    include: {
+      specilization: true,
+    },
+    where: {
+      //   OR: [
+      //     {
+      //       fullName: {
+      //         contains: searchTerm,
+      //         mode: 'insensitive',
+      //       },
+      //     },
+      //     {
+      //       specilization: {
+      //         name: {
+      //           contains: searchTerm,
+      //           mode: 'insensitive',
+      //         },
+      //       },
+      //     },
+      //     {
+      //       qualification: {
+      //         contains: searchTerm,
+      //         mode: 'insensitive',
+      //       },
+      //     },
+      //   ],
+      specilization: {
+        name: {
+          equals: filterData.specialization as string,
+          mode: 'insensitive',
+        },
+      },
+      qualification: {
+        equals: filterData.qualification as string,
+        mode: 'insensitive',
+      },
+    },
+    take: limit,
+    skip: (page - 1) * limit,
+    orderBy: {
+      [sortBy]: sortOrder,
+    },
+  })
+  const total = await prisma.doctor.count()
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  }
+}
+
+const getDoctor = async (id: string): Promise<Doctor | null> => {
+  const result = await prisma.doctor.findUnique({
+    where: { id },
+  })
+  return result
+}
+export const doctorService = { createDoctor, getDoctors, getDoctor }
